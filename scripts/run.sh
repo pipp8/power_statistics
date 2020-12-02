667472b0a6ec7b3a25fa37c9cd7ebd794d974b2d
pipp8 <cattaneo@unisa.it>
2020-12-02 16:51:29 +0100

    launch script updated

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,16 @@
-#! /bin/sh
 
-spark-submit --class it.unisa.di.bio.DatasetBuilder \
-  --master yarn --deploy-mode cluster --driver-memory 4g \
-  --num-executors 8 --executor-memory 13g --executor-cores 3 \
-  target/powerstatistics-1.0-SNAPSHOT.jar \
-	/user/cattaneo/data/powerstatistics 10000 100000 5 0.05 ACCCCGT
+#! /bin/bash
+
+# SeqLen=1000
+# while [ $SeqLen -le 15000 ]; do
+#    echo "running for len= $SeqLen"
+#    ((SeqLen+=1000))
+# done
+
+    
+    spark-submit --class it.unisa.di.bio.DatasetBuilder \
+		 --master yarn --deploy-mode client --driver-memory 16g \
+		 --num-executors 4 --executor-memory 27g --executor-cores 7 \
+		 target/powerstatistics-1.0-SNAPSHOT.jar \
+		 data/dataset5-1000 detailed yarn 1000000 1000000 100000
+
