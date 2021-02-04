40d41a627e5b3e32541ab1005ccb8cb18d0d02bd
Pipp8 <cattaneo@unisa.it>
2021-02-04 18:56:58 +0100

    Aggiornati gli script per il plot dei dati versione dataset-5

--- R-Scripts/runRMkDown.r
+++ R-Scripts/runRMkDown.r
@@ -2,4 +2,4 @@ library(rmarkdown)
 
 Sys.setenv(PATH = paste(Sys.getenv("PATH"), "/usr/local/texlive/2018/bin/x86_64-darwin", sep=.Platform$path.sep)) 
 
-render("RisultatiTestKolmogorov.rmd")
+render("RisultatiTestKolmogorov.Rmd")
