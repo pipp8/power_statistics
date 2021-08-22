551e1cbc22b9513415405c16dc514dcdd4de4d6d
pipp8 <cattaneo@unisa.it>
2021-08-22 19:13:54 +0200

    Aggiunto sccript per il calcolo di jaccard con cafe + jellyfish

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,10 +7,33 @@
 #    ((SeqLen+=1000))
 # done
 
-    
-    spark-submit --class it.unisa.di.bio.DatasetBuilder \
-		 --master yarn --deploy-mode client --driver-memory 16g \
-		 --num-executors 4 --executor-memory 27g --executor-cores 7 \
-		 target/powerstatistics-1.0-SNAPSHOT.jar \
-		 data/dataset5-1000 detailed yarn 1000000 1000000 100000
+start=100000
+last=10000000
+step=200000
+
+case $# in
+    1)
+	start=$1
+	;;
+    2)
+	start=$1
+	last=$2
+	;;
+    3)
+	start=$1
+	last=$2
+	step=$3
+	;;
+    *)
+	echo "Usage: $0 [start[last[step]]]"
+	exit -1
+	;;
+esac
+
+spark-submit --class it.unisa.di.bio.DatasetBuilder \
+	     --master yarn --deploy-mode client --driver-memory 16g \
+	     --num-executors 4 --executor-memory 27g --executor-cores 7 \
+	     target/powerstatistics-1.0-SNAPSHOT.jar \
+	     data/test detailed yarn $start $last $step
+
 
