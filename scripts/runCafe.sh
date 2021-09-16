925f4df2b636a3126320bcdd6fbd90780a24da6a
pipp8 <cattaneo@unisa.it>
2021-09-16 13:24:05 +0200

    runCafe modified

--- scripts/runCafe.sh
+++ scripts/runCafe.sh
@@ -1,87 +1,33 @@
 #! /bin/bash
 
-maxSeqNum=1000
-k=8
-prefix=Uniform
-tmp=/tmp/aa.tmp
+tmp=/tmp/aa.$$
 path=data/dataset5-1000
 
+if [ $# -ne 4 ]; then
+    echo "errore cattivo numero di argomenti $#"
+    echo "Usage: runCafe.sh seq1 seq2 k results"
+    exit -1
+fi
 
+seq1=$1
+seq2=$2
+k=$3
+results=$4
+results2=$(dirname $4)/$(basename $4 .csv)-values.csv
 
-for len in $(seq 200000 200000 10000000); do
+echo "Processing $(basename $seq1 .fasta), $(basename $seq2 .fasta), k=$3, results=$(basename $results)"
 
-    for prefix in Uniform Uniform-T1 MotifRepl-U PatTransf-U; do
-	    
-	for gamma in 100 050 010; do
-
-	    case $prefix in
-		Uniform | Uniform-T1)
-		    postfix=".fasta"
-		    file=$prefix-1000.$len$postfix
-		    one=1
-		    ;;
-		MotifRepl-U | PatTransf-U)
-		    postfix=.G=0.$gamma.fasta
-		    file=$prefix-1000.$len$postfix
-		    one=0
-		    ;;
-		*)
-		    echo "Unknown prefix"
-		    exit -1
-		    ;;
-	    esac
-
-	    echo "Downloading file: <$path/$file>"
-	    hdfs dfs -get $path/$file . > /dev/null 2>&1
-	    
-	    echo "Splitting file"
-	    splitFasta.py $file > /dev/null
-		
-	    cd seqDists
-	    
-	    for k in 4 6 8 10; do
-
-		results="../k=$k/dist-k=${k}_$(basename $file .fasta).csv"
-		echo "SEQ1, SEQ2, jaccard" > $results
-		echo "Processing Sequences from $file for k=$k"
-		for i in $(seq 1 $maxSeqNum); do
-		    seqId=$(printf "$prefix-%05d" $i)
-		    if [ "$one" = 1 ]; then
-			seq1=$seqId-A.fasta
-			seq2=$seqId-B.fasta
-		    else
-			seq1=$seqId.G=$gamma-A.fasta
-			seq2=$seqId-G=$gamma-B.fasta
-		    fi			
-		    cafe -R -K $k -J /usr/local/bin/jellyfish -D jaccard -I $seq1,$seq2 > $tmp
-		    line=$(tail -n 5 $tmp | head -1)
-		    f1=$(echo $line | cut -d ' ' -f 1-1)
-		    d=$(echo $line | cut -d ' ' -f 3-3)
-		    if [ "$f1.fasta" != "$seq1" ]; then
-			echo "cafe failed!!! see file $tmp"
-			exit -1
-		    else
-			echo $seq1,$seq2, $d >> $results
-		    fi
-		
-		done # for each pair
-		echo "k = $k terminated"
-	    
-	    done # for each k
-	    
-	    echo "$file processed."
-	    cd ..
-	    rm -fr seqDists
-	    rm $file
-	    
-	    if [ "$one" = "1" ]; then
-		break;
-	    fi
-	
-	done # for each gamma
-    
-    done # for each model
-
-done #for each len
-
+cafe-mod -R -K $k -J /usr/local/bin/jellyfish -D jaccard -I $seq1,$seq2 > $tmp
+line=$(tail -n 5 $tmp | head -1)
+f1=$(echo $line | cut -d ' ' -f 1-1)
+d=$(echo $line | cut -d ' ' -f 3-3)
+if [ "$f1.fasta" != $(basename "$seq1") ]; then
+    echo "cafe failed!!! see file $tmp"
+    exit -1
+else
+    echo $seq1,$seq2, $d >> $results
+    line2=$(tail -n 9 $tmp | head -1)
+    echo $line2 >> $results2
+fi
+rm $tmp
 
