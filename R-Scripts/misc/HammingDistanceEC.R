00cb46de9d77ae330a958ff110423df06dc1185f
Pipp8 <cattaneo@unisa.it>
2021-12-12 21:09:13 +0100

    Added scripts to compute hamming distance HammingDistanceEC.R

--- R-Scripts/misc/HammingDistanceEC.R
+++ R-Scripts/misc/HammingDistanceEC.R
@@ -3,13 +3,12 @@ library(ggplot2)
 library(dplyr)
 
 
-setwd("/Users/pipp8/Universita/Src/IdeaProjects/power_statistics/data/results/Kolmogorov/AnalisiAlternativeModel/seqs")
-#setwd("/Volumes/Catalina/PowerStatistics/Kolmogorov/")
-
-nPairs = 1000
-lenghts = c(200000, 5000000)
+# setwd("/home/cattaneo/results/Escherichiacoli")
+setwd('/Users/pipp8/Universita/Progetti/BioInformatica/Present-Absent/Src')
 
 dfFilename = "HammingDistanceEC-All.df"
+
+gammas <- c(0.005, 0.010, 0.050, 0.100, 0.200, 0.300, 0.500)
 
 if (file.exists(dfFilename)) {
 	cat( sprintf("Data file exists. Loading %s\n", dfFilename))			
@@ -17,61 +16,44 @@ if (file.exists(dfFilename)) {
 	df <- readRDS(file = dfFilename)
 } else {
 
-	df <- data.frame( Name = character(), Dist = double(), len = numeric(), stringsAsFactors=FALSE)
+	df <- data.frame( Name = character(), Gamma = double(), Dist = double(), P = double, len = numeric(), stringsAsFactors=FALSE)
 
 	model = 'EscherichiaColi'
-	f1 <- sprintf("%s.fasta", model)
-	for( g in c(0.01, 0.05, 0.10))	{
-		f2 <- sprintf("%s-G=%.3f.fasta", model, g)
-		con1 = file(f1, "r")
-		con2 = file(f2, "r")
 
-		for( pair in 1:nPairs) {
+	for( g in gammas)	{
+	  f1 <- sprintf("%s.fasta", model)
+	  con1 = file(f1, "r")
+	  f2 <- sprintf("%s-G=%.3f.fasta", model, g)
+		con2 = file(f2, "r")
+    d <- 0
+    totLen <- 0
+		while( TRUE) {
 
 			s1Name = readLines(con1, n = 1)
 			s1 = readLines(con1, n = 1)
-			s2Name = readLines(con1, n = 1)
-			s2 = readLines(con1, n = 1)
-		
-			d <- StrDist(s1, s2, method = 'hamming')
-			df[nrow(df) + 1,] <- list( 'NM', d[1]/len, len)
+			s2Name = readLines(con2, n = 1)
+			s2 = readLines(con2, n = 1)
+
+			if (length(s1Name) == 0 || length(s2Name) == 0) {
+			  break
+			}
+			if (startsWith(s1, ">") || startsWith(s2, ">")) {
+			  stop("Malformed input file")
+			}
 			
-			cat( sprintf("%5d /%5d\r", pair, nPairs))
+			# compute hamming distance
+			d <- d + StrDist(s1, s2, method = 'hamming')
+			totLen <- totLen + length(s1)
 		}
 		close(con1)
-		cat( sprintf("%s.  done.\n", 'Uniform'))
-	
-		for( model in c('MotifRepl-U', 'PatTransf-U')) {
-			
-			for( g in c(0.01, 0.05, 0.10)) {
-		
-				gVal <- sprintf(".G=%.3f", g)
-				k1 <- sprintf("%s%s", if (model == 'MotifRepl-U') 'MR' else 'PT', gVal)
-						
-				f2 <- sprintf("%s-%d.%d%s.fasta", model, nPairs, len, gVal)
-				con2 = file(f2, "r")
-			
-				for( pair in 1:nPairs) {
-		
-					s1AMName = readLines(con2, n = 1)
-					s1AM = readLines(con2, n = 1)
-					s2AMName = readLines(con2, n = 1)
-					s2AM = readLines(con2, n = 1)
-				
-					d <- StrDist(s1AM, s2AM, method = 'hamming')
-					df[ nrow(df) + 1,] <- list( k1, d[1]/len, len)
-					
-					cat( sprintf("%5d /%5d\r", pair, nPairs))
-				} # for all pairs
-	
-				close(con2)
-				cat( sprintf("\n%s ok\n", f2))	
-			} # for all gamma
-			cat( sprintf("%s.  done.\n", model))
-		} # for all models
-	} # for all lenngth
+		close(con2)
+		df[nrow(df) + 1,] <- list( model, g, d[1]/totLen, d[2], totLen)
+		cat( sprintf("%s. G=%f d = %d. done.\n", model, g, d))
+	}
 	saveRDS( df, file = dfFilename)
 }
+
+stop()
 
 df$Name = factor(df$Name, levels = c('NM', 'MR.G=0.010','MR.G=0.050','MR.G=0.100', 'PT.G=0.010','PT.G=0.050','PT.G=0.100'))
 
