0c2f7974cc4198377537593aa314f4a9e76bc31f
Pipp8 <cattaneo@unisa.it>
2022-01-23 21:41:46 +0100

    Added python script to compute Present/Absent values

--- scripts/PresentAbsent.py
+++ scripts/PresentAbsent.py
@@ -6,6 +6,7 @@ import sys
 import glob
 import subprocess
 import csv
+import math
 from filelock import Timeout, FileLock
 import numpy as np
 
@@ -26,8 +27,10 @@ nPairs = 1000
 nTests = 3
 minK = 4
 maxK = 12
+sketchSize = 1000
 outFile = 'PresentAbsentData.csv'
 hdfsDataDir = 'data/dataset7-1000'
+
 
 
 def extractKmers( dataset, k, seq):
@@ -52,6 +55,7 @@ def extractKmers( dataset, k, seq):
     # for f in glob.glob(kmcOutputPrefix+'*'):
     #    os.remove(f)
     return vect
+
 
 
 def loadKmerList( file):
@@ -80,7 +84,6 @@ def loadKmerList( file):
 # run jaccard on sequence pair ds with kmer of length = k
 def runPresentAbsent( ds, k):
 
-    print( ds)
     m = re.search(r'^(.*)-(\d+)\.(\d+)(.*)', ds)
     if (m is not None):
         model = m.group(1)
@@ -107,14 +110,121 @@ def runPresentAbsent( ds, k):
     NMax = pow(4, k)
     M01M10 = leftCnt + rightCnt
     M01M10M11 = bothCnt + M01M10
-    absentCnt = NMax - (A + B + C) # M01M10M11
+    absentCnt = NMax - (A + B + C) # NMax - M01M10M11
     D = absentCnt
     # (M10 + M01) / (M11 + M10 + M01)
-    # jaccardDistance = M01M10 / float(M01M10M11)
+
+    # Anderberg dissimilarity => Anderberg = 1 - (A/(A + B) + A/(A + C) + D/(C + D) + D/(B + D))/4
+    try:
+        anderberg = 1 - (A/float(A + B) + A/float(A + C) + D/float(C + D) + D/float(B + D))/4.0
+    except ZeroDivisionError:
+        anderberg = 'UnDefined'
+
+    # Antidice dissimilarity => Antidice = 1 - A/(A + 2(B + C))
+    try:
+        antidice = 1 - A / float(A + 2.0 * (B + C))
+    except ZeroDivisionError:
+        antidice = 'UnDefined'
+
+    # Dice dissimilarity => Dice = 1 - 2A/(2A + B + C)
+    try:
+        dice = 1 - 2*A / float(2.0*A + B + C)
+    except ZeroDivisionError:
+        dice  = 'UnDefined'
+    # Gower dissimilarity => Gower = 1 - A x D/sqrt(A + B) x(A + C) x (D + B x (D + C)
+    try:
+        gower = 1 - A * D / math.sqrt((A + B) * (A + C) * (D + B * (D + C)))
+    except ZeroDivisionError:
+        gower = 'UnDefined'
+
+    # Hamman dissimilarity => Hamman = 1 - [((A + D) - (B + C))/N]2
+    try:
+        hamman = 1 - math.pow((((A + D) - (B + C)) / float(NMax)), 2.0)
+    except ZeroDivisionError:
+        hamman = 'UnDefined'
+
+    # Hamming dissimilarity => Hamming = (B + C)/N
+    try:
+        hamming = (B + C)/ NMax
+    except ZeroDivisionError:
+        hamming = 'UnDefined'
+
+    # Jaccard dissimilarity => Jaccard = 1 - A/(N - D)
+    try:
+        jaccard = 1 - A / (NMax - D)
+    except ZeroDivisionError:
+        jaccard = 'UnDefined'
+
     jaccardDistance = 1 - min( 1.0, A / float(NMax - D))
-    
-    header = ['model', 'gamma', 'seqLen', 'pairId', 'k', 'Jaccard Distance', 'A', 'B', 'C', 'D', 'Nmax']
-    data = [model, gamma, seqLen, pairId, k, jaccardDistance, bothCnt, leftCnt, rightCnt, absentCnt, NMax]
+
+    # Kulczynski dissimilarity => Kulczynski = 1 - (A/(A + B) + A/(A + C)) / 2
+    try:
+        kulczynski = 1 - (A / float(A + B) + A / float(A + C)) / 2.0
+    except ZeroDivisionError:
+        kulczynski = 'UnDefined'
+
+    # Matching dissimilarity => Matching = 1 - (A + D)/N
+    try:
+        matching = 1 - (A + D) / NMax
+    except ZeroDivisionError:
+        matching = 'UnDefined'
+
+    # Ochiai dissimilarity => Ochiai = 1 - A/sqrt(A + B) x (A + C)
+    try:
+        ochiai = 1 - A / math.sqrt((A + B) * (A + C))
+    except ZeroDivisionError:
+        ochiai = 'UnDefined'
+
+    # Phi dissimilarity => Phi = 1 - [(A x  B x  C x D)/sqrt(A + B) x (A + C) x (D + B) x (D + C)]2
+    try:
+        phi = 1 - math.pow((A * B * C * D)/ math.sqrt((A + B) * (A + C) * (D + B) * (D + C)), 2.0)
+    except ZeroDivisionError:
+        phi = 'UnDefined'
+
+    # Russel dissimilarity => Russel = 1 - A/N
+    try:
+        russel = 1 - A / NMax
+    except ZeroDivisionError:
+        russel = 'UnDefined'
+
+    # Sneath dissimilarity => Sneath = 1 - 2(A + D)/(2 x (A + D) + (B + C))
+    try:
+        sneath = 1 - 2.0 * (A + D) / (2.0 * (A + D) + (B + C))
+    except ZeroDivisionError:
+        sneath = 'UnDefined'
+
+    # Tanimoto dissimilarity => Tanimoto = 1 - (A + D)/((A + D) + 2(B + C))
+    try:
+        tanimoto = 1 - (A + D) / float((A + D) + 2.0 * (B + C))
+    except ZeroDivisionError:
+        tanimoto = 'UnDefined'
+
+    # Yule dissimilarity => Yule = 1 - [(A x D - B x C)/(A x D + B x C)]2
+    try:
+        yule = 1 - math.pow(((A * D - B * C) / float(A * D + B * C)), 2.0)
+    except ZeroDivisionError:
+        yule = 'UnDefined'
+
+    # run mash on the same sequence pair
+    inputDS1 = '%s/%s-A.fasta' % (splitFasta.seqDistDir, ds)
+    inputDS2 = '%s/%s-B.fasta' % (splitFasta.seqDistDir, ds)
+
+    # run kmc on the first sequence
+    cmd = "mash sketch -s %d -k %d %s" % (sketchSize, k, inputDS1)
+    p = subprocess.Popen(cmd.split())
+    p.wait()
+    print("cmd: %s returned: %s" % (cmd, p.returncode))
+    cmd = "mash sketch -s %d -k %d %s" % (sketchSize, k, inputDS2)
+    p = subprocess.Popen(cmd.split())
+    p.wait()
+    print("cmd: %s returned: %s" % (cmd, p.returncode))
+    cmd = "mash dist %s.msh %s.msh" % (inputDS1, inputDS2)
+    out = subprocess.check_output(cmd.split())
+    print("cmd: %s returned: %s" % (cmd, out))
+
+    mashResults = out.split()
+
+    data = [model, gamma, seqLen, pairId, k, A, B, C, D, NMax, anderberg, antidice, dice, gower, hamman, hamming, jaccard, jaccardDistance, kulczynski, matching, ochiai, phi, russel, sneath, tanimoto, yule, mashResults[2], mashResults[3], mashResults[4] ]
 
     lock = FileLock(outFile + '.lck')
     try:
@@ -124,6 +234,7 @@ def runPresentAbsent( ds, k):
         if (not os.path.exists( outFile)):
             f = open(outFile, 'w')
             writer = csv.writer(f)
+            header = ['model', 'gamma', 'seqLen', 'pairId', 'k', 'A', 'B', 'C', 'D', 'N', 'Anderberg', 'Antidice', 'Dice', 'Gower', 'Hamman', 'Hamming', 'Jaccard', 'jaccardDistance', 'Kulczynski', 'Matching', 'Ochiai', 'Phi', 'Russel', 'Sneath', 'Tanimoto', 'Yule', 'Mash Pv', 'Mash Distance', 'A/N']
             writer.writerow(header)
         else:
             f = open(outFile, 'a')
