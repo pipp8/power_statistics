fd8149bc05769cc2f10c81ee6c516a7860684bf4
Pipp8 <cattaneo@unisa.it>
2023-10-06 21:56:19 +0200

    Aggiunti script python per la manipolazione delle sequenze (MakeSimilar.py e CheckBasisDitribution.py)

--- Py-Scripts/runPresentAbsent.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-
-spark-submit --master yarn --deploy-mode client --driver-memory 27g \
-	     --num-executors 48 --executor-memory 27g --executor-cores 7 \
-	     PySparkPresentAbsent4.py 10000 8-32
-
-
