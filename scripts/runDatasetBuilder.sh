cd09cb1103d527cc8db1a75b0bdee49f0a16c168
umbfer <umbfer@users.noreply.github.com>
2024-11-15 15:01:04 +0100

    Added the possibility to specify whether the code should be executed in local mode or yarn mode

--- scripts/runDatasetBuilder.sh
+++ scripts/runDatasetBuilder.sh
@@ -11,8 +11,8 @@ prefixPath="data"
 
 
 
-if (( $# != 7)); then
-    echo "Usage: $0 uniform|eColiShuffled fromLen toLen step #pairs geneSize patternSize"
+if (( $# != 8 )); then
+    echo "Usage: $0 uniform|eColiShuffled fromLen toLen step #pairs geneSize patternSize local|yarn"
     exit -1
 else
     model=$1
@@ -22,6 +22,8 @@ else
     numPairs=$5
     geneSize=$6
     patternSize=$7
+    executionMode=$8
+
     if [[ $model == "uniform" ]]; then 
        destDir=$(printf "%s/uniform-%d-%d/len=%d" $prefixPath $patternSize $numPairs $start)
     elif [[ $model == "eColiShuffled" ]]; then
@@ -30,13 +32,18 @@ else
        echo "please specify model = uniform|eColiShuffled"
        exit -1
     fi
+
+    if [[ $executionMode != "local" && $executionMode != "yarn" ]]; then
+        echo "Please specify execution mode = local|yarn"
+        exit -1
+    fi
 fi
 
 spark-submit --class it.unisa.di.bio.DatasetBuilder \
-	     --master yarn --deploy-mode client --driver-memory 16g \
+	     --master $executionMode --deploy-mode client --driver-memory 16g \
 	     --num-executors 4 --executor-memory 27g --executor-cores 7 \
 	     target/PowerStatistics-1.3-SNAPSHOT-jar-with-dependencies.jar \
-	     --output $destDir --generator $model --mode yarn\
+	     --output $destDir --generator $model  --mode $executionMode\
              --from-len $start --to-len $last --step $step \
              --pairs $numPairs  --geneSize $geneSize \
              --patternSize $patternSize -f
