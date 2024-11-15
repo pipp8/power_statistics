75ce710fcecd386c4b3bd48584de88c887732b4a
umbfer <umbfer@users.noreply.github.com>
2024-11-15 16:51:58 +0100

    Added the possibility to specify whether the code should be executed in local mode or yarn mode. When in local mode, files are read from the local file system

--- scripts/runPresentAbsent.sh
+++ scripts/runPresentAbsent.sh
@@ -1,18 +1,28 @@
 #! /bin/bash
 
-if (( $# != 2)); then
-    echo "Usage: $0 seqLen mainDataDir"
+
+if (( $# != 3 )); then
+    echo "Usage: $0 seqLen mainDataDir local|yarn"
     exit -1
 else
     seqLen=$1
     dataDir=$2
+    executionMode=$3
+
+    if [[ $executionMode != "local" && $executionMode != "yarn" ]]; then
+        echo "Please specify execution mode as either 'local' or 'yarn'"
+        exit -1
+    fi
 fi
 
+cmd="spark-submit --master $executionMode --deploy-mode client --driver-memory 27g"
 
-cmd="spark-submit --master yarn --deploy-mode client --driver-memory 27g \
-	     --num-executors 48 --executor-memory 27g --executor-cores 7 \
-	     Py-Scripts/PySparkPresentAbsent4.py $seqLen $dataDir"
+# Configure executors only for yarn mode
+if [[ $executionMode == "yarn" ]]; then
+    cmd="$cmd --num-executors 48 --executor-memory 27g --executor-cores 7"
+fi
 
+cmd="$cmd Py-Scripts/PySparkPresentAbsent4.py $seqLen $dataDir"
 
 logFile="run-${seqLen}-$(date '+%s').log"
 
